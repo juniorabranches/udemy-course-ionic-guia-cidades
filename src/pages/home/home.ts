d085d9c91fe0340512c78a23349de4123dd7556a
Junior Abranches <juniorabranches@gmail.com>
2018-08-24 00:12:48 -0400

    Secao 7 - Aula 5

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,9 +7,30 @@ import { IonicPage, NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  cidades: Array<{cidade: string, estado: string, imagem: string}>
   constructor(public navCtrl: NavController) {
-
+    this.cidades = [
+      {
+        cidade: 'Cuiabá',
+        estado: 'Mato Grosso',
+        imagem: 'assets/imgs/cidades/matoGrosso.jpg'
+      },
+      {
+        cidade: 'Rio de Janeiro',
+        estado: 'Rio de Janeiro',
+        imagem: 'assets/imgs/cidades/rioDeJaneiro.jpg'
+      },
+      {
+        cidade: 'São Paulo',
+        estado: 'São Paulo',
+        imagem: 'assets/imgs/cidades/saoPaulo.jpg'
+      },
+      {
+        cidade: 'Goiania',
+        estado: 'Goiás',
+        imagem: 'assets/imgs/cidades/goias.jpg'
+      },                
+    ]
   }
 
 }
